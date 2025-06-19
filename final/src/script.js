aaefc3981bf1548b506bde8d1a1f2d0f6253bdf0
Yusseiin <yusseiin.95@gmail.com>
2025-06-19 12:15:50 +0200

    The end

--- final/src/script.js
+++ final/src/script.js
@@ -11,8 +11,24 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 3, 3, 3)
+
+const geometry = new THREE.BufferGeometry()
+
+const count = 50
+
+const positionsArray = new Float32Array(count * 3 * 3 )
+
+for (let index = 0; index < positionsArray.length; index++) {
+    positionsArray[index] = Math.random() - 0.5
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+
+
+geometry.setAttribute('position', positionsAttribute)
+
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
